6e0910119ef297f67e4d114b2e5c333bb9674442
Marcos Cardoso <vrcmarcos@gmail.com>
2016-08-17 10:26:46 -0300

    Fixing version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Paginated-Response',
-    version='1.2.0',
+    version='1.0.0',
     url='https://github.com/vrcmarcos/flask-paginated-response',
     license='MIT',
     author='Marcos Cardoso',
